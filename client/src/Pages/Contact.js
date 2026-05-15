2e6dc286b546601d701296a62855f893956d6b5c
Orbal Digital Academy <155068331+orbaldigitalacademy@users.noreply.github.com>
2026-05-15 08:05:37 +0100

    Update Contact.js

--- client/src/Pages/Contact.js
+++ client/src/Pages/Contact.js
@@ -7,7 +7,7 @@ function Contact() {
 
       <p><strong>Phone:</strong> +1723-801-9887</p>
       <p><strong>Email:</strong> gloriaomowole@yahoo.com</p>
-      <p><strong>Address:</strong> Nigeria</p>
+      <p><strong>Address:</strong> USA</p>
 
       <h3>Or call to place your order directly.</h3>
     </div>
