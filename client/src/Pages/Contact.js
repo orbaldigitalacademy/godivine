4b32691a0e037d2b259c0c971285a74daf622228
Orbal Digital Academy <155068331+orbaldigitalacademy@users.noreply.github.com>
2026-05-15 08:48:03 +0100

    Update Contact.js

--- client/src/Pages/Contact.js
+++ client/src/Pages/Contact.js
@@ -3,13 +3,13 @@ import React from "react";
 function Contact() {
   return (
     <div className="page">
-      <h1>Contact Us</h1>
+      <h1> Contact Us </h1>
 
       <p><strong>Phone:</strong> +1723-801-9887</p>
       <p><strong>Email:</strong> gloriaomowole@yahoo.com</p>
       <p><strong>Address:</strong> USA</p>
 
-      <h3call to place your order directly.</h3>
+      <h3> call to place your order directly.</h3>
     </div>
   );
 }
